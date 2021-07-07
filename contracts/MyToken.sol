a464950b4e0ea091e038cfb345d00ae8b7b5d551
Dornat <mrpolosukhin@gmail.com>
2021-07-07 11:29:10 +0300

    Add mintable token functionality to CAPPU.

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,14 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("StarDucks Cappuccino Token", "CAPPU") {
-        _mint(msg.sender, initialSupply);
+contract MyToken is ERC20PresetMinterPauser {
+    uint8 MAX_MINTABLE_AMOUNT = 42;
+
+    mapping (address => uint256) private _mintedAmount;
+
+    constructor() ERC20PresetMinterPauser("StarDucks Cappuccino Token", "CAPPU") {
+    }
+
+    function mint(address to, uint256 amount) public override {
+        require(_mintedAmount[to] + amount <= MAX_MINTABLE_AMOUNT, "CAPPU: Maximum mintable amount for address is reached.");
+        super.mint(to, amount);
+        _mintedAmount[to] = _mintedAmount[to] + amount;
     }
 
     function decimals() public view virtual override returns (uint8) {
         return 0;
+    }
+
+    function burn(uint256) public override pure {
+        revert("CAPPU: Those CAPPU tokens are too precious to be burned!");
+    }
+
+    function burnFrom(address, uint256) public override pure {
+        revert("CAPPU: Those CAPPU tokens are too precious to be burned!");
     }
 }
--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -7,36 +7,66 @@ const {BN} = web3.utils
 const {expect} = chai
 
 contract('MyToken Test', async (accounts) => {
-  const [deployer, alice, bob] = accounts
+  const [deployer, alice] = accounts
 
   beforeEach(async () => {
-    this.myToken = await MyToken.new(process.env.INITIAL_TOKENS);
+    this.myToken = await MyToken.new();
   })
 
-  it('should put all tokens in first account', async () => {
+  it('should be possible to mint tokens', async () => {
     const instance = this.myToken
-    const totalSupply = await instance.totalSupply()
+    await instance.mint(deployer, 42)
 
-    expect(await instance.balanceOf(deployer)).to.be.bignumber.equal(totalSupply)
+    expect(await instance.balanceOf(deployer)).to.be.bignumber.equal(new BN(42))
+  })
+
+  it('should not be possible to mint a lot of tokens', async () => {
+    const instance = this.myToken
+    await expect(instance.mint(deployer, 43)).to.be.rejectedWith(Error)
+    await instance.mint(deployer, 42)
+    await expect(instance.mint(deployer, 1)).to.be.rejectedWith(Error)
+    expect(await instance.balanceOf(deployer)).to.be.bignumber.equal(new BN(42))
+    await instance.mint(alice, 1)
+    await instance.mint(alice, 3)
+    await expect(instance.mint(alice, 39)).to.be.rejectedWith(Error)
+    await instance.mint(alice, 10)
+    expect(await instance.balanceOf(alice)).to.be.bignumber.equal(new BN(14))
+  })
+
+  it('should not be possible to mint tokens from account without minter role', async () => {
+    const instance = this.myToken
+    await expect(instance.mint(alice, 42, {from: alice})).to.be.rejectedWith(Error)
+  })
+
+  it('should be possible to grant minter role to alice and mint tokens on her behalf', async () => {
+    const instance = this.myToken
+    const minterRole = await instance.MINTER_ROLE()
+    await expect(instance.grantRole(minterRole, alice)).to.be.fulfilled
+    await expect(instance.mint(alice, 42, {from: alice})).to.be.fulfilled
+  })
+
+  it('should not be possible to burn tokens', async () => {
+    const instance = this.myToken
+    await instance.mint(deployer, 42)
+    await expect(instance.burn(42)).to.be.rejectedWith(Error)
   })
 
   it('should be possible to send tokens between accounts', async () => {
     const instance = this.myToken
-    const totalSupply = await instance.totalSupply()
-    expect(await instance.balanceOf(deployer)).to.be.bignumber.equal(totalSupply)
+    await instance.mint(deployer, 42)
 
     await instance.transfer(alice, 1)
-    expect(await instance.balanceOf(deployer)).to.be.bignumber.equal(totalSupply.sub(new BN(1)))
+    expect(await instance.balanceOf(deployer)).to.be.bignumber.equal(new BN(41))
     expect(await instance.balanceOf(alice)).to.be.a.bignumber.equal(new BN(1))
   })
 
   it('should not be possible to send more tokens than available in total', async () => {
     const instance = this.myToken
-    const totalSupply = await instance.totalSupply()
+    await instance.mint(deployer, 42)
     const balanceOfDeployer = await instance.balanceOf(deployer)
 
     await instance.transfer(alice, 1)
     await expect(instance.transfer(alice, new BN(balanceOfDeployer + 1))).to.be.rejectedWith(Error)
-    expect(await instance.balanceOf(deployer)).to.be.bignumber.equal(totalSupply.sub(new BN(1)))
+    expect(await instance.balanceOf(deployer)).to.be.bignumber.equal((new BN(balanceOfDeployer - 1)))
   })
 })
